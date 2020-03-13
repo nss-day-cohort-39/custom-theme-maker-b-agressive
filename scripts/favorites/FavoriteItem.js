1d14ea7ffc529e8294796d26864bdf947f11f275
durrjp <durrjp1@gmail.com>
2020-03-13 15:33:38 -0500

    fixing the dates to read YY/MM/DD. for some reason

--- scripts/favorites/FavoriteItem.js
+++ scripts/favorites/FavoriteItem.js
@@ -6,7 +6,18 @@ export const FavoriteItem = favorite => `
         <p>
             <img class="favoriteItem_image" src="${favorite.url}" />
         </p>
-        <p class="favoriteItem_date">Date: ${favorite.timestamp}</p>
+        <p class="favoriteItem_date">Date: ${readableDate(favorite.timestamp)}</p>
     </section>
 `
+
+const readableDate = (timestamp) => {
+
+    const coolDate = new Date(timestamp).toLocaleDateString('en-US')
+    const splitDate = coolDate.split("/")
+    const fixedYear = splitDate[2].split("")
+    const finalData = fixedYear.slice(2)
+    const joinedYear = finalData.join("")
+    return `${joinedYear}/${splitDate[0]}/${splitDate[1]}`
+
+}
 
